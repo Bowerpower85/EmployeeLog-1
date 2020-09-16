396df022e92acd63bd830eafa875b99bcd25f4d9
Douglas Mitchell <roxonus@gmail.com>
2020-09-16 15:05:58 -0700

    wrote a function to run the prompt

--- server.js
+++ server.js
@@ -16,3 +16,29 @@ const connection = mysql.createConnection({
     runSearch();
   });
   
+  function runSearch() {
+    inquirer
+      .prompt({
+        name: "create",
+        type: "list",
+        message: "Create a [Department], a [Role], an [Employee] or [View] the following:",
+              choices: ["Department", "Role", "Employee", "View"]
+          })
+          .then(function(promptOptions){
+              if (promptOptions.create === "Department") {
+                  addDepartment();
+              }
+              else if(promptOptions.create === "Role") {
+                  addRole();
+              }
+              else if(promptOptions.create === "Employee") {
+                  addEmployee();
+              }
+              else if(promptOptions.create === "View") {
+                  viewData();
+              }
+              else{
+                  connection.end();
+              }
+          })
+  }
