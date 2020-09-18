f3f796ec0667807d608ac3390ee43303529b38f3
Douglas Mitchell <roxonus@gmail.com>
2020-09-18 05:26:28 -0700

    wrote a function in server.js to add a role

--- server.js
+++ server.js
@@ -66,3 +66,41 @@ const connection = mysql.createConnection({
         );
       });
   }
+
+
+  function addRole() {
+    inquirer
+      .prompt([
+        {
+          name: "title",
+          type: "input",
+          message: "What role title are you adding?",
+        },
+        {
+          name: "salary",
+          type: "input",
+          message: "What is the salary for this position?",
+        },
+        {
+          name: "departmentId",
+          type: "input",
+          message: "What departmentID are we adding?",
+        }
+      ])
+      .then(function(answer) {
+
+        connection.query(
+          "INSERT INTO role SET ?",
+          {
+            title: answer.title,
+            salary: answer.salary,
+            department_id: answer.departmentId
+          },
+          function(err) {
+            if (err) throw err;
+            console.log("Role Added!");
+            runSearch();
+          }
+        );
+      });
+  }
